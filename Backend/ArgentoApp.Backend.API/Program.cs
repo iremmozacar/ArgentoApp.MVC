17ac4b19a35623888b0f9378fda88bc285cf0539
iremozacar <iremozacaar@gmail.com>
2024-09-29 20:59:47 +0300

    New classes added.

--- Backend/ArgentoApp.Backend.API/Program.cs
+++ Backend/ArgentoApp.Backend.API/Program.cs
@@ -25,7 +25,7 @@ builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<ICartRepository, CartRepository>();
 builder.Services.AddScoped<ICartItemRepository, CartItemRepository>();
 builder.Services.AddScoped<IOrderRepository, OrderRepository>();
-
+builder.Services.AddScoped<ICommentRepository, CommentRepository> ();
 
 
 
@@ -34,7 +34,9 @@ builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<ICartService, CartService>();
 builder.Services.AddScoped<ICartItemService, CartItemService>();
-builder.Services.AddScoped<IOrderService, OrderService>();
+builder.Services.AddScoped<IOrderService, OrderService>
+();
+builder.Services.AddScoped<ICommentRepository, CommentRepository>();
 
 
 
--- Backend/ArgentoApp.Backend.Shared/obj/Debug/net8.0/ArgentoApp.Backend.Shared.AssemblyInfo.cs
+++ Backend/ArgentoApp.Backend.Shared/obj/Debug/net8.0/ArgentoApp.Backend.Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArgentoApp.Backend.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc390269fe3634c15606119667c67e7167b1ede0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbc7aae347576e021a562584f6cb82816f20b003")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArgentoApp.Backend.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArgentoApp.Backend.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
